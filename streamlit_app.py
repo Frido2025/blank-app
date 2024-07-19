b4fd47a253aab145a16431ed4c26125823659de4
Frido2025 <fva3009@icloud.com>
2024-07-19 16:22:59 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,6 @@ st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
 #header
-st.header("This is a Header", divider = "gray")
+st.header(
+    "This is a Header", divider = "gray"
+)
