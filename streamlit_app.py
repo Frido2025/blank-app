a521eb4f69d20ad6eb22acc333c15ffa1c08461c
Frido2025 <fva3009@icloud.com>
2024-07-19 16:30:18 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit as st
 
+
+# class navigation:
+    # def __init__(self,
+
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
